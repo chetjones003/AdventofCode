7e399f923992f506f7d80d3bc283dc33490d7ce6
Chet Jones <chetjones003@gmail.com>
2023-11-02 19:08:35 -0500

    feat: finished part1

--- src/year_2015/day3.ts
+++ src/year_2015/day3.ts
@@ -13,21 +13,34 @@ type Vec2 = {
   y: number;
 };
 
-export function part1(): Vec2 {
-  const arr = input.trim().split("");
+function countUniques(arr: number[][]) {
+  const uniques = Array.from(new Set(arr.map((a) => a.join("|"))), (s) =>
+    s.split("|").map(Number),
+  );
+  return [...uniques].length;
+}
+
+export function part1(): number {
+  const arr: string[] = input.trim().split("");
+  const vistited_coordinates: number[][] = [[0, 0]];
   const startingPoint: Vec2 = { x: 0, y: 0 };
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === Direction.NORTH) {
       startingPoint.y += 1;
+      vistited_coordinates.push([startingPoint.x, startingPoint.y]);
     } else if (arr[i] === Direction.EAST) {
       startingPoint.x += 1;
+      vistited_coordinates.push([startingPoint.x, startingPoint.y]);
     } else if (arr[i] === Direction.SOUTH) {
       startingPoint.y -= 1;
+      vistited_coordinates.push([startingPoint.x, startingPoint.y]);
     } else if (arr[i] === Direction.WEST) {
       startingPoint.x -= 1;
+      vistited_coordinates.push([startingPoint.x, startingPoint.y]);
     }
   }
-  return startingPoint;
+
+  return countUniques(vistited_coordinates);
 }
 
 console.log("part1: ", part1());
