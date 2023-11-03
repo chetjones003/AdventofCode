b0082f36202159cfe97dcd53df7a66d7ac9b9b5f
Chet Jones <chetjones003@gmail.com>
2023-11-02 20:39:15 -0500

    feat: finish part2

--- src/year_2015/day3.ts
+++ src/year_2015/day3.ts
@@ -1,5 +1,6 @@
 import { readFileSync } from "fs";
 const input: string = readFileSync("./input/year_2015/day3.txt", "utf8");
+const stringArr = input.trim().split("");
 
 enum Direction {
   NORTH = "^",
@@ -13,17 +14,24 @@ type Vec2 = {
   y: number;
 };
 
-function countUniques(arr: number[][]) {
-  const uniques = Array.from(new Set(arr.map((a) => a.join("|"))), (s) =>
-    s.split("|").map(Number),
-  );
-  return [...uniques].length;
+export function part1(): number {
+  const vistited_coordinates = getAllCoordinates(stringArr);
+  return countUniques(vistited_coordinates);
 }
 
-export function part1(): number {
-  const arr: string[] = input.trim().split("");
+export function part2(): number {
+  const santasCoordinates = getAllCoordinates(splitStringForSanta(stringArr));
+  const robosCoordinates = getAllCoordinates(splitStringForRobo(stringArr));
+  const allCoords = santasCoordinates.concat(robosCoordinates);
+  const allUniques = countUniques(allCoords);
+
+  return allUniques;
+}
+
+function getAllCoordinates(arr: string[]): number[][] {
   const vistited_coordinates: number[][] = [[0, 0]];
   const startingPoint: Vec2 = { x: 0, y: 0 };
+
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === Direction.NORTH) {
       startingPoint.y += 1;
@@ -40,7 +48,35 @@ export function part1(): number {
     }
   }
 
-  return countUniques(vistited_coordinates);
+  return vistited_coordinates;
+}
+
+function countUniques(arr: number[][]) {
+  const uniques = Array.from(new Set(arr.map((a) => a.join("|"))), (s) =>
+    s.split("|").map(Number),
+  );
+  return [...uniques].length;
+}
+
+function splitStringForSanta(input: string[]): string[] {
+  let santasString: string[] = [];
+  for (let i = 0; i < input.length; i++) {
+    santasString = input.filter((_, index) => {
+      return (index + 1) % 2 === 0;
+    });
+  }
+  return santasString;
+}
+
+function splitStringForRobo(input: string[]): string[] {
+  let robosString: string[] = [];
+  for (let i = 0; i < input.length; i++) {
+    robosString = input.filter((_, index) => {
+      return index % 2 === 0;
+    });
+  }
+  return robosString;
 }
 
 console.log("part1: ", part1());
+console.log("part2: ", part2());
